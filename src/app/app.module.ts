76b7521a035dd58e8efa1f33043b2a739c13c385
Lu Khei <kheiboardwarrior@gmail.com>
2020-11-27 09:37:20 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { ApiFormComponent } from './components/api-form.component';
+import { CountryListComponent } from './components/country-list.component';
+import { ResultsComponent } from './components/results.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './components/home.component';
 
+const ROUTES: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'results', component: ResultsComponent},
+  {path: '**', redirectTo:'/', pathMatch: 'full'}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApiFormComponent,
+    CountryListComponent,
+    ResultsComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule.forRoot(ROUTES)
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
